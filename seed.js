9fa95b3640b2c9d10093953686fd98fb1952371d
Rairocha <52248306+Rairocha@users.noreply.github.com>
2023-08-07 13:39:26 -0300

    working for all senators

--- seed.js
+++ seed.js
@@ -1,41 +1,39 @@
 require('dotenv').config()
-
 const axios = require('axios')
 const mongoose = require('mongoose')
-
 const Politician = require('./models/Politician')
 
+
+
+function range(start, end) {
+    if(start === end) return [axios.get(`https://api.propublica.org/congress/v1/${start}/senate/members.json`
+    , {
+        headers:  'X-API-Key: ecUtaApjr5p3Vbt8F8BGWnv1NEAHbY6fSQhGLfTO'
+    })];
+    return [axios.get(`https://api.propublica.org/congress/v1/${start}/senate/members.json`
+    , {
+        headers:  'X-API-Key: ecUtaApjr5p3Vbt8F8BGWnv1NEAHbY6fSQhGLfTO'
+    }), ...range(start + 1, end)];
+}
+const promises = range(80,117)
+console.log(promises)
+const results  = async () => {
+    try {
+        const result = await Promise.all(promises)
+        const politicians = await Politician.create(result.map((r)=>r.data.results[0].members).flat()) 
+    } catch (err) {
+        console.log("Error creating politicians", err)
+    }
+}
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
+  .then(results())
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
   });
 
-
-axios.get("https://api.propublica.org/congress/v1/116/senate/members.json"
-, {
-    headers:  'X-API-Key: ecUtaApjr5p3Vbt8F8BGWnv1NEAHbY6fSQhGLfTO'
-})
-    .then((results) => {
-        Politician.create(results.data.results[0].members)
-            .then((createdPoliticians) => {
-                console.log("Created ====>", createdPoliticians)
-                mongoose.connection.close()
-            })
-            .catch((err) => {
-                console.log("Error creating politicians", err)
-            })
-
-
-    })
-    .catch((err) => {
-        console.log("Error connecting with Axios", err)
-        next(err)
-
-    
- 
-    })
+//mongoose.connection.close()
